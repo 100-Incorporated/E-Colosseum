4f0cf89584b28ee1a6e63fbae822b2409c1c5bee
aung306 <92897562+aung306@users.noreply.github.com>
2023-02-06 21:06:29 -0500

    frontend

---
+++ frontend/src/app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const login = document.querySelector('.login');
+
+login.addEventListener('click', function() {
+    //now move onto the next page
+    location.href="login.html";
+ 
+});
