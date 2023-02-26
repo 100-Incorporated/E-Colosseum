fa182836d5ed4790e5312bd2ac366ada0eeccc03
Sophia Huerta <smhuerta26@gmail.com>
2023-02-26 15:30:07 -0500

    Update main.ts
    updated main.ts

--- Colosseum/src/main.ts
+++ Colosseum/src/main.ts
@@ -3,5 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+
+const bootstrapPromise = platformBrowserDynamic().bootstrapModule(AppModule);
+
+bootstrapPromise.then(success => console.log(`Bootstrap success`))
   .catch(err => console.error(err));
