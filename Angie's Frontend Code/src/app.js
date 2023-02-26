56109924af61c026ddc861cd149f25cbd02f6600
Angela Ung <angelaung@ufl.edu>
2023-02-26 15:22:21 -0500

    added login removed non-working files

--- Angie's Frontend Code/src/app.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const login = document.querySelector('.login');
-
-login.addEventListener('click', function() {
-    //now move onto the next page
-    location.href="login.html";
- 
-});
