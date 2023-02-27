02a432a9509e765ae96460075dafee3031c05d8a
Angela Ung <angelaung@ufl.edu>
2023-02-26 21:35:02 -0500

    linked pages in website

--- Colosseum/src/main.ts
+++ Colosseum/src/main.ts
@@ -2,9 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
-
+const loginpage = document.querySelector('.loginpage');
 const bootstrapPromise = platformBrowserDynamic().bootstrapModule(AppModule);
 
 bootstrapPromise.then(success => console.log(`Bootstrap success`))
   .catch(err => console.error(err));
+
