effc0850a5c777c4343ca10294911bf9e17bbbbc
Sophia Huerta <smhuerta26@gmail.com>
2023-02-27 20:49:32 -0500

    made the sign up login and play as guest thing different

--- Colosseum/src/main.ts
+++ Colosseum/src/main.ts
@@ -3,6 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 const loginpage = document.querySelector('.loginpage');
+const signuppage = document.querySelector('.signup');
+const guestpage = document.querySelector('.gameScreen');
 const bootstrapPromise = platformBrowserDynamic().bootstrapModule(AppModule);
 
 bootstrapPromise.then(success => console.log(`Bootstrap success`))
