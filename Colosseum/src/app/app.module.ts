c1d6fcb2d6019ef0eb16354983188ad80725e772
Jeffrey Drew <98627285+jeffreydrew@users.noreply.github.com>
2023-02-28 16:19:50 -0500

    proper routing

--- Colosseum/src/app/app.module.ts
+++ Colosseum/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+    {path: 'sign-up', component: SignUpComponent},
+    {path: 'login', component: LoginComponent},
+  ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
